bbb146acb090987238a9c94ca62fa34d6808b954
deepak surya <deepak.surya@buildingblocks.la>
2023-02-07 18:22:01 +0530

    models in separate file

---
+++ src/db/db.modules.ts
@@ -0,0 +1,10 @@
+
+import { Comment } from 'src/comments/entities/comment.entity';
+import { Post } from 'src/post/entities/post.entity';
+import { User } from 'src/users/user.entity';
+
+export const ALL_MODELS = [
+    User,
+    Post,
+    Comment
+]
